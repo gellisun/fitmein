5a4e5765db683d7f9e76eccbc08ee92f4f27aa03
gellisun <uengyaf@hotmail.it>
2023-08-05 17:44:56 +0400

    user model, profile model, urls.py set up

--- fitmein/settings.py
+++ fitmein/settings.py
@@ -128,6 +128,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+LOGIN_REDIRECT_URL = '/profile/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.core.validators import MinValueValidator
+from django.urls import reverse
+from django.contrib.auth.models import User
 
-# Create your models here.
+GENDER = (('M', 'Male'),('F', 'Female'),('O', 'Other'))
+
+ACTIVITIES = (('RU','Running'),('WL', 'Weight Lifting'),('GC','Group Classes'),('BR', 'Bike Riding'),('TE','Tennis'),('SQ','Squash'),('WA','Walking'),('BA','Badminton'),('SW','Swimming'),('WA','Walking'),('HI','Hiking'), ('P', 'Pilates'), ('SU', 'Surfing'), ('SK', 'Skateboarding'))
+
+def get_profile_image_filepath(self, filename):
+    return f'profile_images/{self.pk}/{"profile_image.png"}'
+
+class Profile(models.Model):
+    gender = models.CharField(max_length=1, choices=GENDER)
+    age = models.IntegerField(validators=[MinValueValidator(0)])
+    location = models.CharField()
+    is_couch_potato = models.BooleanField(default=True)
+    favorites = models.CharField(max_length=2, choices=ACTIVITIES)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'user_id': self.id})
+    
+
+class Badges(models.Model):
+    name = models.CharField()
+    icon = models.ImageField(max_length=255, upload_to=get_profile_image_filepath)
+    profile = models.ManyToManyField(Profile)
+
+def get_profile_image_filename(self):
+    return str(self.profile_image)[str(self.profile_image).index(f'profile_images/{self.pk}/'):]
