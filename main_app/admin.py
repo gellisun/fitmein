716868a5a47919bf172d5ba452b99d02fdf338b3
hannah <hannah.l.curran@gmail.com>
2023-08-06 14:42:01 +0100

    add profile form

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+# from .models import User, Profile, Badges
+
+# admin.site.register(User)
+# admin.site.register(Profile)
+# admin.site.register(Badges)
 
 # Register your models here.
--- main_app/views.py
+++ main_app/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
 
-from .models import Profile, Badges
+from .models import Profile, Badges, User
+from .forms import ProfileForm
 
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
@@ -18,19 +19,36 @@ def about(request):
 
 @login_required
 def profile(request):
+  profile, created = Profile.objects.get_or_create(user=request.user)
+  if request.method == 'POST':
+    profile_form = ProfileForm(request.POST, instance=profile)
+    if profile_form.is_valid():
+      profile_form.save()
+  else:
+    profile_form = ProfileForm(instance=profile)
 
-  return render(request, 'user/profile.html')
+  context = {'profile': profile, 'profile_form': profile_form,}
+  return render(request, 'user/profile.html', context)
 
 def signup(request):
   error_message = ''
+  # profile, created = Profile.objects.get_or_create(user=request.user)
   if request.method == 'POST':
+    # profile_form = ProfileForm(request.POST, instance=profile)
     form = UserCreationForm(request.POST)
+    # if form.is_valid() and profile_form.is_valid():
     if form.is_valid():
       user = form.save()
+      # profile_form.save()
       login(request, user)
       return redirect('profile')
     else:
       error_message = 'Invalid sign up - try again!'
+      profile_form = ProfileForm()
   form = UserCreationForm()
-  context = {'form': form, 'error_message': error_message}
+  context = {'form': form, 'error_message': error_message,}
   return render(request, 'registration/signup.html', context)
+
+   
+
+  
