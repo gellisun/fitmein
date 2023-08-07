d2c52fe2292be95bcd1870f9ab48fa92ac30327a
hannah <hannah.l.curran@gmail.com>
2023-08-07 11:25:49 +0100

    work on form add gitignore @gellisun

--- main_app/views.py
+++ main_app/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
 
-from .models import Profile, Badges
+from .models import Profile, Badges, User
+from .forms import ProfileForm
 
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
@@ -19,7 +20,29 @@ def about(request):
 @login_required
 def profile(request):
 
-  return render(request, 'user/profile.html')
+ try:
+    profile = Profile.objects.get(user=request.user)
+    profile_exists = True
+ except Profile.DoesNotExist:
+    profile = None
+    profile_exists = False
+
+ if request.method == 'POST':
+    profile_form = ProfileForm(request.POST, instance=profile)
+    if profile_form.is_valid():
+        profile_form.save()
+    else:
+        new_profile = profile_form.save(commit=False)
+        new_profile.user = request.user
+        new_profile.save()
+ else:
+    if profile_exists:
+      profile_form = ProfileForm(instance=profile)
+    else:
+      profile_form = ProfileForm()
+
+ context = {'profile': profile, 'profile_form': profile_form,}
+ return render(request, 'user/profile.html', context)
 
 def signup(request):
   error_message = ''
