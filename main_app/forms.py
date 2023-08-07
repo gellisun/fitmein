0ed50b9502882254a41b91234084553296561b21
hannah <hannah.l.curran@gmail.com>
2023-08-07 20:55:05 +0100

    add comments form on profile view with full CRUD

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,7 +1,13 @@
 from django.forms import ModelForm
-from .models import Profile 
+from .models import Profile, Comment
+from django import forms
 
 class ProfileForm(ModelForm):
     class Meta:
         model = Profile
         fields = ['location', 'age', 'gender', 'is_couch_potato', 'favorites']
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['content']
--- main_app/views.py
+++ main_app/views.py
@@ -3,9 +3,10 @@ import requests
 import json
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
+from django.urls import reverse_lazy
 
-from .models import Profile, Badges, User
-from .forms import ProfileForm
+from .models import Profile, Badges, User, Comment
+from .forms import ProfileForm, CommentForm
 
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
@@ -38,12 +39,13 @@ def profile(request):
         new_profile.user = request.user
         new_profile.save()
  else:
+    comments = Comment.objects.filter(user=profile.user)
     if profile_exists:
       profile_form = ProfileForm(instance=profile)
     else:
       profile_form = ProfileForm()
 
- context = {'profile': profile, 'profile_form': profile_form,}
+ context = {'profile': profile, 'profile_form': profile_form, 'comments': comments}
  return render(request, 'user/profile.html', context)
 
 def signup(request):
@@ -68,3 +70,47 @@ def match(request):
   location_data = json.loads(location_data_one) #loading location data one
   return render(request, 'user/match.html', {'data': location_data, 'ip': ip_data })
 
+class CommentListView(LoginRequiredMixin, ListView):
+    model = Comment
+    template_name = 'user/profile.html'
+    context_object_name = 'comments'
+    ordering = ['-date']
+    # Filter comments for the current user
+    def get_queryset(self):
+        return super().get_queryset().filter(user=self.request.user)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['comments'] = self.get_queryset()
+        return context
+    
+     
+class CommentCreateView(LoginRequiredMixin, CreateView):
+    model = Comment
+    fields = ['content']
+    template_name = 'user/create_comment.html'
+    success_url = reverse_lazy('profile')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+    
+        
+
+class CommentUpdateView(LoginRequiredMixin, UpdateView):
+    model = Comment
+    fields = ['content']
+    template_name = 'user/edit_comment.html'
+    success_url = reverse_lazy('profile')
+
+    def get_queryset(self):
+        return super().get_queryset().filter(user=self.request.user)
+
+class CommentDeleteView(LoginRequiredMixin, DeleteView):
+    model = Comment
+    template_name = 'user/delete_comment.html'
+    success_url = reverse_lazy('profile')
+
+    def get_queryset(self):
+        return super().get_queryset().filter(user=self.request.user)
+    
