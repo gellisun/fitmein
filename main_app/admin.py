85e19dbbbab7760730823a1f7f7e885a3b25df7e
gellisun <uengyaf@hotmail.it>
2023-08-08 21:24:45 +0400

    css

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,11 +1,3 @@
 from django.contrib import admin
-from .models import CustomUser
-from django.contrib.auth.admin import UserAdmin
 
-
-class CustomUserAdmin(UserAdmin):
-    list_display = ['username', 'email', 'age', 'gender', 'city']
-
-admin.site.register(CustomUser, CustomUserAdmin)
-# admin.site.register(Badges)
 # Register your models here.
