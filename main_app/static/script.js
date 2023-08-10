c9627050537efc6d8916b73697e291f42e225745
Lucas Neno <lucasneno@gmail.com>
2023-08-10 15:47:29 +0100

    Merged Main (final) into my local lucas branch

--- main_app/static/script.js
+++ main_app/static/script.js
@@ -20,44 +20,12 @@ function success(position) {
     const longitude = position.coords.longitude;
     console.log('success')
     console.log(latitude)
-    // latitudeDisplay.innerHTML = latitude
-    // longitudeDisplay.innerHTML = longitude
 
     window.location.assign(`http://localhost:8000/my_match/${latitude}/${longitude}/`)
-    // sendToServer(latitude, longitude)
+
 }
 
 function error() {
     console.log('error')
     alert("Unable to retrieve your location.");
-}
-
-// Function to get CSRF token from cookies
-function getCsrf(csrf) {
-    const cookieValue = document.cookie.match('(^|;)\\s*' + csrf + '\\s*=\\s*([^;]+)');
-    return cookieValue ? cookieValue.pop() : '';
-}
-console.log(getCsrf('csrftoken'))
-
-function sendToServer(latitude, longitude){
-    const data = { 'latitude': latitude, 'longitude': longitude };
-    
-    fetch('/', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'X-CSRFToken': getCsrf('csrftoken'),
-        },
-        body: JSON.stringify(data),
-    })
-        .then((response) => {
-            if (response.ok) {
-                console.log('Location data sent successfully.');
-            } else {
-                console.error('Failed to send location data to server.');
-            }
-        })
-        .catch(error => {
-            console.error('Error sending location data:', error);
-        });
 }
